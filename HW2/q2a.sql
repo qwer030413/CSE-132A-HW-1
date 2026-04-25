41ea7dd943f365b83dd60b3e08c29a22bb9c7e4e
Chris <comosjay@gmail.com>
2026-04-25 15:52:27 -0700

    almost done!

---
+++ HW2/q2a.sql
@@ -0,0 +1,5 @@
+UPDATE loan SET type = 'temp' WHERE type = 'jumbo mortgage';
+
+UPDATE loan SET type = 'jumbo mortgage' WHERE type = 'student';
+
+UPDATE loan SET type = 'student' WHERE type = 'temp';
