c7d6dcf8dedadf12210584ae70768c06344e8c0b
qwer030413 <comosjay@gmail.com>
2026-04-26 11:17:25 -0700

    again

--- HW2/q1f.sql
+++ HW2/q1f.sql
@@ -1,5 +1,6 @@
+-- Active: 1777227377359@@127.0.0.1@3306
 SELECT c1.name AS name1, c2.name AS name2
-FROM customer c1 JOIN customer c2 ON c1.name > c2.name
+FROM customer c1 JOIN customer c2 ON c1.name < c2.name
 --see if there is a loan taken by c1 but not by c2
 WHERE NOT EXISTS(
     SELECT b1.lno FROM borrower b1 WHERE b1.cname = c1.name
