74778d42752bd0af69abaab491cd10060c6c3ac3
qwer030413 <comosjay@gmail.com>
2026-05-17 19:45:32 -0700

    ra is so hard

--- HW2/q1d.sql
+++ HW2/q1d.sql
@@ -1,3 +1,8 @@
+-- List the customers who took every type of loan 
+-- (at least one loan from every type). 
+-- The output schema should be (name).
+
+
 SELECT c.name FROM customer c  
 JOIN borrower b on c.name = b.cname JOIN loan l on l.no = b.lno
 GROUP BY c.name HAVING COUNT(DISTINCT l.type) = (
